1f1211c7ca4580fc89f9b29790f5b329fab37ccc
Kian Ho <hui.kian.ho@googlemail.com>
2015-03-15 01:56:58 +1100

    Added code to remove newlines.

---
+++ sentiment-analysis/make_train_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+
+Author:
+    Kian Ho <hui.kian.ho@gmail.com>
+
+Description:
+    Read a list of paths to text files from stdin and concatenate their contents
+    into a minimal-quoted csv format.
+
+Usage:
+    make_train_data.py -l LABEL [-o CSV] [-e ENCODING]
+
+Options:
+    -l, --label LABEL           Label to assign each review instance.
+    -o, --output CSV            Write output to .csv file.
+    -e, --encoding ENCODING     Input file encoding [default: utf-8].
+
+"""
+
+import os
+import sys
+import re
+import codecs
+import pandas
+
+from docopt import docopt
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    label = int(opts["--label"])
+    records = []
+    for fn in ( ln.strip() for ln in sys.stdin ):
+        with open(fn) as f:
+            blob = codecs.getreader(opts["--encoding"])(f).read()
+        records.append((label, blob))
+
+    df = pandas.DataFrame.from_records(records, columns=None)
+    df.to_csv(opts["--output"] if opts["--output"] else sys.stdout,
+            index=None, header=False, encoding="utf-8")
