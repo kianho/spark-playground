3ea8d40055d47847c45a20718b0e71b937810763
Kian Ho <hui.kian.ho@googlemail.com>
2015-03-15 15:43:38 +1100

    Added code to check "stability" of label assignments.

--- sentiment-analysis/snippets/doc-id-feat-request.py
+++ sentiment-analysis/snippets/doc-id-feat-request.py
@@ -8,21 +8,20 @@ sc = SparkContext(conf=conf)
 # list of (<label>,<text>) tuples
 docs = [(1, "Hello hello world dog"), (2, "Hello WORLD world cat"), 
         (3, "cat dog")]
-rdd = sc.parallelize(docs, 2)
+rdd = sc.parallelize(docs)
 
 htf = HashingTF(1000)
 idf = IDF()
 
 # keep the label, tokenize, then compute the term-frequency hashes.
+# convert the result to collection of LabelPoints.
 toks_rdd = rdd.map(
     lambda (label, text):
         LabeledPoint(label, htf.transform(text.lower().split())))
-labels_rdd = toks_rdd.map(lambda x: x.label)
 
-# keep only the TF vectors:
-tfvec_rdd = toks_rdd.map(lambda x: x.features) 
-tfidf_rdd = idf.fit(tfvec_rdd).transform(tfvec_rdd)
+idf.fit(toks_rdd).transform(toks_rdd)
 
-# Is the original ordering of entries in `toks_rdd` preserved in `tdfidf_rdd`?
-print toks_rdd.map(lambda x: x[1]).collect()
-print labels_rdd.zip(tfidf_rdd).collect()
+labels_before = rdd.map(lambda (label, text): label).collect()
+labels_after = toks_rdd.map(lambda x: int(x.label)).collect()
+
+# labels_before == labels_after guaranteed ?
